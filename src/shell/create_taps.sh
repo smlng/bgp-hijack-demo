3ed4cb6acd040005d793026c116d548ef060ee65
smlng <s@mlng.net>
2015-01-22 23:05:51 +0100

    updated config, for single and chained bgpmon

--- src/shell/create_taps.sh
+++ src/shell/create_taps.sh
@@ -18,9 +18,12 @@ ifconfig tap6 up 192.168.1.6 netmask 255.255.255.255
 ifconfig tap7 up 192.168.1.7 netmask 255.255.255.255
 
 ## network interface for bgpmon
+openvpn --mktun --dev tap100
 openvpn --mktun --dev tap101
-ifconfig tap101 up 192.168.1.101 netmask 255.255.255.255
 openvpn --mktun --dev tap102
-ifconfig tap102 up 192.168.1.102 netmask 255.255.255.255
 openvpn --mktun --dev tap103
+
+ifconfig tap100 up 192.168.1.100 netmask 255.255.255.255
+ifconfig tap101 up 192.168.1.101 netmask 255.255.255.255
+ifconfig tap102 up 192.168.1.102 netmask 255.255.255.255
 ifconfig tap103 up 192.168.1.103 netmask 255.255.255.255
