c763fe7cfa6262eed5c8ce5d9d007579f090a9ad
smlng <s@mlng.net>
2015-03-11 10:03:56 +0100

    demo helper

--- src/shell/S03demo_webserver.sh
+++ src/shell/S03demo_webserver.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 BASEDIR=$(pwd)
+IPADDR=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
 DEMOHOST="0.0.0.0"
 DEMOPORT="8000"
 BGPMON=/usr/local/bin/bgpmon
 # check if directory correct
 [ ! -d "$(pwd)/etc" ] && { echo "Missing config directory (etc)! Run from repo root!"; exit 1; }
 [ ! -d "$(pwd)/src/python/www" ] && { echo "Missing source directory (src/python/www)! Run from repo root!"; exit 1; }
+[ ! -d "$(pwd)/src/html" ] && { echo "Missing source directory (src/html)! Run from repo root!"; exit 1; }
 echo " - BASEDIR           [ OK ]"
+env WSIP=$IPADDR sed -e "s/ws:\/\/.*:5002/ws:\/\/$WSIP:5002/g" -i '' ./src/html/monitoring.html
+echo " - set websocket IP  [ OK ]"
 sleep 5
 while read SIGNAL; do
     case "$SIGNAL" in
