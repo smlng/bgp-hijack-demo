bfb271f9e0247763f2ed20a80db48d462be1a34c
smlng <s@mlng.net>
2015-08-12 09:13:57 +0200

    removed unused hijack URLs

--- src/shell/hijack.sh
+++ src/shell/hijack.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 WWWPATH="/var/www"
-WEBSITES=(spiegel.de web.de zeit.de)
+#WEBSITES=(spiegel.de web.de zeit.de)
+WEBSITES=(zeit.de)
 
 for w in ${WEBSITES[@]}; do
-	if [ "$1" == "add" ] ; then
-		INDEXHTML="$WWWPATH/$w/htdocs/index.html"
-		INDEXHACK="$WWWPATH/$w/htdocs/index_hack.html"
-		cp -p $INDEXHACK $INDEXHTML
-	else
-		INDEXHTML="$WWWPATH/$w/htdocs/index.html"
-		INDEXGOOD="$WWWPATH/$w/htdocs/index_good.html"
-		cp -p $INDEXGOOD $INDEXHTML
-	fi
+    if [ "$1" == "add" ] ; then
+        INDEXHTML="$WWWPATH/$w/htdocs/index.html"
+        INDEXHACK="$WWWPATH/$w/htdocs/index_hack.html"
+        cp -p $INDEXHACK $INDEXHTML
+    else
+        INDEXHTML="$WWWPATH/$w/htdocs/index.html"
+        INDEXGOOD="$WWWPATH/$w/htdocs/index_good.html"
+        cp -p $INDEXGOOD $INDEXHTML
+    fi
 done
